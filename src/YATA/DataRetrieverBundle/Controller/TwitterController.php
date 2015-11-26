8048a6e0ec7f207f10c05add1c3b1306f0659d4e
Steven Yung <stevenyung@MacBook-Pro-de-Steven.local>
2015-11-26 18:02:59 +0100

    add rb + begining of controller

--- src/YATA/DataRetrieverBundle/Controller/TwitterController.php
+++ src/YATA/DataRetrieverBundle/Controller/TwitterController.php
@@ -22,14 +22,24 @@ class TwitterController extends Controller
         return $this->render('YATADataRetrieverBundle:Default:index.html.twig', array('form' => $form->createView()));
     }
 
+    public function get_tweets($form)
+    {
+      $form = json_encode($form);
+      #$test = system("ruby ../API/TweetsRetriever.rb");
+      $test = system("ls ../src/YATA/DataRetrieverBundle/API/TweetsRetriever.rb");
+      return ($test);
+    }
+
     public function tweetAction(Request $request)
     {
 
         if ($request->getMethod() == 'POST')
         {
             $form = $request->request->get('form');
+            $tweet = $this->get_tweets($form);
             return $this->render('YATADataRetrieverBundle:Default:tweet.html.twig',
-                array('username' => $form["username"])
+                array('username' => $form["username"],
+                      'tweet' => $tweet)
             );
         }
 
