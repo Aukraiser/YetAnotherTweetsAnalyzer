eae2662c231c073da65f31031191f407823905a5
is-sam <mejdoubi.issam@gmail.com>
2015-11-26 18:26:21 +0100

    Update Search

--- src/YATA/DataRetrieverBundle/Object/Search.php
+++ src/YATA/DataRetrieverBundle/Object/Search.php
@@ -8,9 +8,28 @@ class Search
     /**
      * @var String Twitter username
      * @Assert\NotBlank()
-     * @Assert\Length(min=3)
      */
     private $username;
+
+    /**
+     * @var String Keywords to search for
+     */
+    private $keywords;
+
+    /**
+     * @var String Hashtags to search for
+     */
+    private $hashtags;
+
+    /**
+     * @var String Tweet language
+     */
+    private $lang;
+
+    /**
+     * @var String Country location of the tweet
+     */
+    private $country;
 
     /**
      * @return String
@@ -28,6 +47,68 @@ class Search
         $this->username = $username;
     }
 
+    /**
+     * @return String
+     */
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
 
+    /**
+     * @param String $keywords
+     */
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHashtags()
+    {
+        return $this->hashtags;
+    }
+
+    /**
+     * @param mixed $hashtags
+     */
+    public function setHashtags($hashtags)
+    {
+        $this->hashtags = $hashtags;
+    }
+
+    /**
+     * @return String
+     */
+    public function getLang()
+    {
+        return $this->lang;
+    }
+
+    /**
+     * @param String $lang
+     */
+    public function setLang($lang)
+    {
+        $this->lang = $lang;
+    }
+
+    /**
+     * @return String
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param String $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
 
 }
