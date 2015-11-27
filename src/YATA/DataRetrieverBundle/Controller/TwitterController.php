64ea8e283864ad5624331d2a224fb231445b31cc
Aukraiser <quifouma.samuel@live.fr>
2015-11-27 16:30:27 +0100

    Twitter Tweets Recuperation functionnal

--- src/YATA/DataRetrieverBundle/Controller/TwitterController.php
+++ src/YATA/DataRetrieverBundle/Controller/TwitterController.php
@@ -5,19 +5,20 @@ namespace YATA\DataRetrieverBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use YATA\DataRetrieverBundle\Object\Search;
+use Buzz\Browser;
+use Buzz\Client\Curl;
+use Twitter\OAuth2\Consumer;
 
 class TwitterController extends Controller
 {
 
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $search = new Search();
         $form = $this->createFormBuilder($search)
             ->setAction($this->generateUrl('yata_data_retriever_tweet'))
             ->setMethod('POST')
-            ->add('username', 'text', array('required' => false))
-            ->add('keywords', 'text', array('required' => false))
-            ->add('hashtags', 'text', array('required' => false))
+            ->add('searchParams', 'text', array('required' => true))
             ->add('lang', 'choice', array(
                 'choices' => array(
                     ''      => 'All',
@@ -27,32 +28,70 @@ class TwitterController extends Controller
                 ),
                 'required' => false
             ))
-            ->add('country', 'text', array('required' => false))
+            ->add('locale', 'text', array('required' => false))
+            ->add('resultType', 'choice', array(
+                'choices' => array(
+                    'mixed' => 'Mixed',
+                    'recent' => 'Recent',
+                    ),
+                    'required' => false
+                ))
+            ->add('count', 'text', array('required' => false))
+            ->add('includeEntities', 'text', array('required' => false))
             ->add('send', 'submit', array('label' => 'Get Tweets'))
             ->getForm();
             
-            $r = new Request();
-            $r->create("https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi", "GET");
-            $test = $r->getContent();
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $data = $form->getData();
+            return $this->redirect($this->generateUrl('yata_data_retriever_tweet', array('data' => $form)));
+        }
 
         return $this->render('YATADataRetrieverBundle:Default:index.html.twig',
-                             array('form' => $form->createView(),
-                                   'test' => $test));
+                            array('form' => $form->createView()
+                        ));
     }
 
     public function tweetAction(Request $request)
     {
-
-        if ($request->getMethod() == 'POST')
-        {
-            $form = $request->request->get('form');
-            unset($form['send']);
-            unset($form['_token']);
-            return $this->render('YATADataRetrieverBundle:Default:tweet.html.twig',
-                array('data' => json_encode($form))
-            );
-        }
-
+        //Creating data to call Twitter API
+        $consumer_key = $this->container->getParameter('twitter_app_id');
+        $consumer_secret = $this->container->getParameter('twitter_secret');
+        $searchUrlParams = "";
+        $curl = new Curl();
+        $curl->setVerifyPeer(false);
+        
+        //Getting form data
+        $data = $request->request->get('form');
+        $searchParams = $data['searchParams'];
+        $tabSearch = explode(" ", $searchParams);
+        foreach ($tabSearch as $elem)
+            $searchUrlParams .= $elem;
+        $lang = $data['lang'];
+        $locale = $data['locale'];
+        $resultType = $data['resultType'];
+        $count = $data['count'];
+        $includeEntities = $data['includeEntities'];
+        
+        //Getting tweets
+        $client = new Browser($curl);
+        $consumer = new Consumer($client, $consumer_key, $consumer_secret);
+        $query = $consumer->prepare('/1.1/search/tweets.json',
+                                    'GET',
+                                    array(
+                                        'q' => $searchUrlParams,
+                                        'lang' => $lang,
+                                        'locale' => $locale,
+                                        'resultType' => $resultType,
+                                        'count' => $count,
+                                        'includeEntities' => $includeEntities
+                                    ));
+        $results = $consumer->execute($query);
+        //$resultsDecode = json_decode($results);
+        
+        return $this->render('YATADataRetrieverBundle:Default:tweet.html.twig', array('data' => $results));
+        
         return $this->redirect($this->generateUrl('yata_data_retriever_homepage'));
     }
 }
